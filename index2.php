82ff5b969bd4762246467b3371a36b17b9fe859e
preetsshah <48907607+preetsshah@users.noreply.github.com>
2020-03-30 10:29:06 +0530

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,62 @@
+<?php
+	session_start();
+	// require_once '../init.php';
+	$_SESSION['q']='';
+?>
+
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Ajax-Trials</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h2 align="center">Ajax Live Data Search using Jquery PHP Elasticsearch</h2><br />
+   <div class="form-group">
+    <div class="input-group">
+     <span class="input-group-addon">Search</span>
+     <input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" />
+     
+    </div>
+   </div>
+   <br />
+   <div id="result"></div>
+  </div>
+ </body>
+</html>
+
+
+<script>
+$(document).ready(function(){
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"fetch2.php",
+   method:"POST",
+   data:{query:query},
+   success:function(data)
+   {
+    $('#result').html(data);
+   }
+  });
+ }
+ $('#search_text').keyup(function(){
+  var search = $(this).val();
+  if(search != "")
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+});
+</script>
