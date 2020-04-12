3093684ebdd656ee2cd4924aa9d41e2bd1bfdfe3
Preet Shah <48907607+preetsshah@users.noreply.github.com>
2020-04-12 21:52:08 +0530

    Update index_nptel.php

--- index_nptel.php
+++ index_nptel.php
@@ -1,64 +1,64 @@
 <?php
-	session_start();
+  session_start();
 ?>
-
 <html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>NPTEL Trials</title>
+<head>
+  <title>NPTEL Courses</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css_nptel.css">
- </head>
- <body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+
+</head>
+
+<body>
   <div class="container">
    <br />
    <h2 align="center">Search for any course</h2><br />
    <div class="form-group">
     <div class="input-group">
      <span class="input-group-addon">Search</span>
-     <input type="text" name="search_text" id="search_text" placeholder="Search here..." class="form-control" />
+     <input type="text" name="search_box" id="search_box" class="form-control" placeholder="Type your search query here" />
      
      <br>
-    </div>
    </div>
-   <br />
-   <div id="result"></div>
-  </div>
- </body>
+ </div>
+ <br />
+ <div id="dynamic_content">
+
+ </div>
+</div>
+</body>
 </html>
-
-
 <script>
-$(document).ready(function(){
+  $(document).ready(function(){
 
- load_data();
+    load_data(1);
 
- function load_data(query) 
- {
-  $.ajax({
-   url:"fetch_nptel.php",
-   method:"POST",
-   data:{query:query},
-   //type: JSON, 
-   success:function(data)
-   {
-    $('#result').html(data);
-   }
+    function load_data(page, query = '')
+    {
+      $.ajax({
+        url:"fetch_nptel.php",
+        method:"POST",
+        data:{page:page, query:query},
+        success:function(data)
+        {
+          $('#dynamic_content').html(data);
+        }
+      });
+    }
+
+    $(document).on('click', '.page-link', function(){
+      var page = $(this).data('page_number');
+      var query = $('#search_box').val();
+      load_data(page, query);
+    });
+
+    $('#search_box').keyup(function(){
+      var query = $('#search_box').val();
+      load_data(1, query);
+    });
+
   });
- }
-
- $('#search_text').keyup(function(){
-  var search = $(this).val();
-  if(search != "")
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
-});
 </script>
